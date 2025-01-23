7d0af8a305b4b5ad12a26405e14ccc2eac001009
Amakatus <sachabouton@gmail.com>
2025-01-23 09:16:27 +0100

    shootsuccess

--- include/shoot.h
+++ include/shoot.h
@@ -1,13 +1,15 @@
 #ifndef _SHOOT_H_
 #define _SHOOT_H_
 
+#include "battle_c.h"
 #include "math.h"
 #include "stdbool.h"
+#include <stdio.h>
 
 float Get_Radius_Angle(float x,float y);
 
 float Calculate_Position(float x_a, float x_b, float y_a, float y_b);
 
-bool ShootOnTarget(float x_a, float x_b, float y_a, float y_b);
+BC_ShootResult ShootOnTarget(BC_Connection *connection, float x_a, float x_b, float y_a, float y_b);
 
 #endif
--- src/shoot.c
+++ src/shoot.c
@@ -1,4 +1,5 @@
 #include "shoot.h"
+#include "battle_c.h"
 
 float Get_Radius_Angle(float x,float y){
     return atan2f(y,x); 
@@ -10,6 +11,10 @@ float Calculate_Position(float x_a, float x_b, float y_a, float y_b){
     return Get_Radius_Angle(delta_x, delta_y);
 }
 
-bool ShootOnTarget(float x_a, float x_b, float y_a, float y_b){
-    float radiant = Calculate_Position(x_a, x_b, y_a, y_b);
+BC_ShootResult ShootOnTarget(BC_Connection *connection, float x_a, float x_b, float y_a, float y_b){
+    BC_ShootResult shootresult =  bc_shoot(connection, Calculate_Position(x_a, x_b, y_a, y_b));
+    printf("Succès : %b\n",shootresult.success);
+    printf("Target ID = %d\n",(int) shootresult.target_id);
+    printf("Dégats : %d\n",(int) shootresult.damage_points);
+    printf("Cible détruite ? : %b\n",shootresult.target_destroyed);
 }
