097129b0a929836d1286cd110530244ac5300a28
Gwadaw <116796480+Gwadaw@users.noreply.github.com>
2025-01-30 11:36:48 +0100

    list radar

--- src/main.c
+++ src/main.c
@@ -5,47 +5,103 @@
 #include "stdlib.h"
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-  printf("Initialisation du bot\n");
-  fflush(stdout);
+typedef struct Radar {
+    BC_MapObject radar;
+    struct Radar *next;
+} Radar;
 
-  BC_Connection *conn = bc_connect("5.135.136.236", 8080);
-  printf("Connection réussi\n");
-  fflush(stdout);
+Radar *Radar_list(Radar *head, BC_MapObject *obj) {
+    Radar *new_radar = (Radar *)malloc(sizeof(Radar));
+    if (!new_radar) {
+        fprintf(stderr, "Erreur d'allocation mémoire !\n");
+        return head;
+    }
+    new_radar->radar = *obj;
+    new_radar->next = head;
+    return new_radar;
+}
 
-  BC_WorldInfo world = bc_get_world_info(conn);
-  printf("Arena size\nX: %d, Y: %d\n", world.map_x, world.map_y);
-  fflush(stdout);
+void free_list(Radar *head) {
+    while (head) {
+        Radar *temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
 
-  // Retrive user informations
-  BC_PlayerData data = bc_get_player_data(conn);
-  Print_BC_PlayerData(&data);
-  // Mise à jour des informations
-  // BC_PlayerData data = bc_get_player_data(conn);
-
-  while (true)
-  {
-    BC_List *list = bc_radar_ping(conn);
-
-    // While the linked list is read, print informations in the terminal
+void print_list(Radar *head, const char *type) {
+    printf("\nListe des %s détectés :\n", type);
+    if (!head) {
+        printf("Aucun %s détecté.\n", type);
+        return;
+    }
+    
     int i = 0;
-    do
-    {
-      BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
-      i++;
-      printf("index : %d ,id %d,map_object x = %f, y = %f, type (int/Text) = %d / %s \n",
-             i,
-             map_object->id,
-             map_object->position.x,
-             map_object->position.y,
-             map_object->type,
-             BC_ObjectTypeToString(map_object->type) // Convert from int to text the enum
-      );
-      fflush(stdout);
-    } while (((list = bc_ll_next(list)) != NULL));
-    bc_ll_free(list);
-  }
+    while (head) {
+        i++;
+        printf("   🔹 %s #%d | ID: %d | Position: (x = %f, y = %f)\n",
+               type, i, head->radar.id, head->radar.position.x, head->radar.position.y);
+        head = head->next;
+    }
+}
 
-  return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+    printf("Initialisation du bot\n");
+    fflush(stdout);
+
+    BC_Connection *conn = bc_connect("5.135.136.236", 8080);
+    if (conn == NULL) {
+        fprintf(stderr, "Erreur : Échec de la connexion au serveur.\n");
+        return EXIT_FAILURE;
+    }
+    printf("Connexion réussie\n");
+    fflush(stdout);
+
+    BC_WorldInfo world = bc_get_world_info(conn);
+    printf("Taille d'arene\nX: %d, Y: %d\n", world.map_x, world.map_y);
+    fflush(stdout);
+
+    BC_PlayerData data = bc_get_player_data(conn);
+    Print_BC_PlayerData(&data);
+
+    Radar *player_list = NULL;
+    Radar *wall_list = NULL;
+
+    while (true) {
+        printf("\nScan radar --------------------------------------------------\n");
+        
+        BC_List *list = bc_radar_ping(conn);
+        if (list == NULL) {
+            printf("Aucun objet détecté.\n");
+        } else {
+            free_list(player_list);
+            free_list(wall_list);
+            player_list = NULL;
+            wall_list = NULL;
+
+            BC_List *head = list;
+            do {
+                BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
+
+                if (map_object->type == OT_PLAYER) {
+                    player_list = Radar_list(player_list, map_object);
+                } else if (map_object->type == OT_WALL) {
+                    wall_list = Radar_list(wall_list, map_object);
+                }
+
+            } while ((list = bc_ll_next(list)) != NULL);
+
+            bc_ll_free(head);
+        }
+
+        print_list(player_list, "Joueurs");
+        print_list(wall_list, "Murs");
+
+        usleep(3000000); 
+    }
+
+    free_list(player_list);
+    free_list(wall_list);
+
+    return EXIT_SUCCESS;
 }
