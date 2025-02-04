0cdd9fff878aafcd8a406a05cef23b9397a36a5c
Clément FLAMENT <cflam09@gmail.com>
2025-02-04 11:35:27 +0100

    Add the boot list

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 #include "unistd.h"
 #include "movement.h"
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   BC_Connection *conn = bc_connect("5.135.136.236", 8080);
   printf("Connection réussi\n");
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
   fflush(stdout);
   bc_get_world_info(conn);
   float speed = 20.0;
-  float* speedf = &speed;
+  float *speedf = &speed;
 
   // Retrive user informations
   BC_PlayerData data = bc_get_player_data(conn);
@@ -27,29 +27,46 @@ int main(int argc, char *argv[]) {
 
   Radar *player_list = NULL;
   Radar *wall_list = NULL;
+  Radar *boost_list = NULL;
 
-  while (true) {
+
+  while (!data.is_dead)
+  {
     printf("\nScan radar --------------------------------------------------\n");
     fflush(stdout);
 
     BC_List *list = bc_radar_ping(conn);
-    if (list == NULL) {
+    if (list == NULL)
+    {
       printf("Aucun objet détecté.\n");
       fflush(stdout);
+    }
+    else
+    {
 
-    } else {
       free_list(player_list);
       free_list(wall_list);
+      free_list(boost_list);
       player_list = NULL;
       wall_list = NULL;
-      BC_List *head = list;
-      do {
-        BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
+      boost_list = NULL;
 
-        if (map_object->type == OT_PLAYER) {
+
+      BC_List *head = list;
+      do
+      {
+        BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
+        if (map_object->type == OT_PLAYER)
+        {
           player_list = Radar_list(player_list, map_object);
-          } else if (map_object->type == OT_WALL) {
-            wall_list = Radar_list(wall_list, map_object);
+        }
+        else if (map_object->type == OT_WALL)
+        {
+          wall_list = Radar_list(wall_list, map_object);
+        }
+        else if (map_object->type == OT_BOOST){
+          printf("Boost détecté\n");
+          boost_list = Radar_list(boost_list, map_object);
         }
         /*do {
             printShootInfo(ShootOnTarget(conn, data.position.x, player_list -> radar.position.x, data.position.y, player_list -> radar.position.y));
@@ -64,16 +81,16 @@ int main(int argc, char *argv[]) {
 
     print_list(player_list, "Joueurs");
     fflush(stdout);
-    print_list(wall_list, "Murs");
-    fflush(stdout);
-
-
+    print_list(boost_list, "Boost");
+    //print_list(wall_list, "Murs");
+    //fflush(stdout);
   }
 
   free_list(player_list);
   free_list(wall_list);
+  free_list(boost_list);
+
 
   printf('Fin de script');
   return EXIT_SUCCESS;
-
 }
