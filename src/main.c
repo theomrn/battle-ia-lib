fe926164e6dc918c4d34e09d74807e14caef3d81
Clément FLAMENT <cflam09@gmail.com>
2025-02-04 11:09:42 +0100

    Fix in the main loop, errors with the shoot

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include "radar.h"
-#include "radar.h"
 #include "shoot.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "tool.h"
 #include "unistd.h"
+#include "movement.h"
+
 
 int main(int argc, char *argv[]) {
 
@@ -29,10 +30,13 @@ int main(int argc, char *argv[]) {
 
   while (true) {
     printf("\nScan radar --------------------------------------------------\n");
-        
+    fflush(stdout);
+
     BC_List *list = bc_radar_ping(conn);
     if (list == NULL) {
       printf("Aucun objet détecté.\n");
+      fflush(stdout);
+
     } else {
       free_list(player_list);
       free_list(wall_list);
@@ -47,32 +51,29 @@ int main(int argc, char *argv[]) {
           } else if (map_object->type == OT_WALL) {
             wall_list = Radar_list(wall_list, map_object);
         }
-        do {
+        /*do {
             printShootInfo(ShootOnTarget(conn, data.position.x, player_list -> radar.position.x, data.position.y, player_list -> radar.position.y));
             //movePlayer(conn, player_list -> radar.position.x , player_list -> radar.position.y, data, speedf);
             printf("%d",player_list -> radar.type);
+            fflush(stdout);
             player_list = player_list -> next;
-        } while (player_list -> radar.id != data.id && player_list -> radar.health != 0 );
+        } while (player_list -> radar.id != data.id && player_list -> radar.health != 0 );*/
       } while ((list = bc_ll_next(list)) != NULL);
       bc_ll_free(head);
     }
 
     print_list(player_list, "Joueurs");
+    fflush(stdout);
     print_list(wall_list, "Murs");
+    fflush(stdout);
+
 
   }
 
   free_list(player_list);
   free_list(wall_list);
 
-  float target_x = 23.867260;        // Position de la cible
-  float target_y = 49.615505;        // Position de la cible
-  BC_List *list = bc_radar_ping(conn);
-  sleep(3);
-  printShootInfo(ShootOnTarget(conn, data.position.x, target_x, data.position.y, target_y));
-    sleep(3);
-  printShootInfo(ShootOnTarget(conn, data.position.x, target_x, data.position.y, target_y));
-    sleep(3);
-  printShootInfo(ShootOnTarget(conn, data.position.x, target_x, data.position.y, target_y));
+  printf('Fin de script');
+  return EXIT_SUCCESS;
 
 }
